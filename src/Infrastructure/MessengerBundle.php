7c921787d795785dd2d9d254a994e804bd64fa4f
vitaliy <zndev@ya.ru>
2023-03-10 09:24:54 +0600

    upd

---
+++ src/Infrastructure/MessengerBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Untek\Framework\Messenger\Infrastructure;
+
+use Untek\Core\Kernel\Bundle\BaseBundle;
+
+class MessengerBundle extends BaseBundle
+{
+
+    public function getName(): string
+    {
+        return 'message-bus';
+    }
+
+    public function boot(): void
+    {
+        if ($this->isCli()) {
+            $this->configureFromPhpFile(__DIR__ . '/config/commands.php');
+        }
+    }
+}
