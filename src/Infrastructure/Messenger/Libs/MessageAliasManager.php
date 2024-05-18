671c1434f76e9a9990b7ef719c5f2eb42391e1b4
Vitaliy <theyamshikov@yandex.ru>
2024-05-18 20:09:58 +0500

    upd

--- src/Infrastructure/Messenger/Libs/MessageAliasManager.php
+++ src/Infrastructure/Messenger/Libs/MessageAliasManager.php
@@ -2,6 +2,10 @@
 
 namespace Untek\Framework\Messenger\Infrastructure\Messenger\Libs;
 
+use Untek\Core\Code\Helpers\DeprecateHelper;
+
+DeprecateHelper::hardThrow();
+
 class MessageAliasManager
 {
 
